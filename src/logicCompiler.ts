2205d29362d40fa825bc7636f22022c9b54ceb5f
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-11-28 15:30:50 -0800

    updated logicCompiler with ariaObject 1st rnd

--- src/logicCompiler.ts
+++ src/logicCompiler.ts
@@ -19,49 +19,49 @@ export async function compileLogic(document: vscode.TextDocument): Promise<AriaR
     const anchorsWithoutAriaLabel = await evalAnchors();
 
     anchorsWithoutAriaLabel.forEach((element: string, index: number) => {
-        ariaRecommendations[element] = ariaObject.ariaHidden;
+        ariaRecommendations[element] = ariaObject.elementRole; //UPDATE THIS
     });
 
     // area-maps-alt-text
     const areaMapsWithoutAltText = await checkAreaMapAltText();
 
     areaMapsWithoutAltText.forEach((element: string, index: number) => {
-        ariaRecommendations[element] = 'ARIA Recommendation: [area-maps-alt-text]';
+        ariaRecommendations[element] = ariaObject.areaAltText;
       });
 
     // aria-hidden
     const hiddenAria = await checkAriaHidden();
 
     hiddenAria.forEach((element: string, index: number) => {
-        ariaRecommendations[element] = 'ARIA Recommendation: [aria-hidden]';
+        ariaRecommendations[element] = ariaObject.ariaHidden;
       });
 
     // button-text
     const buttonText = await checkButtonText();
 
     buttonText.forEach((element: string, index: number) => {
-        ariaRecommendations[element] = 'ARIA Recommendation: [button-text]';
+        ariaRecommendations[element] = ariaObject.discernibleButtonText;
     });
 
     // unique-ids
     const duplicateElements = await checkUniqueIds();
 
     duplicateElements.forEach((element: string, index: number) => {
-        ariaRecommendations[element] = 'ARIA Recommendation: [unique-ids]';
+        ariaRecommendations[element] = ariaObject.uniqueIDs;
       });
 
     // img alt text
     const imgAlts = await checkImgAltText();
     
     imgAlts.forEach((element: string, index: number) => {
-        ariaRecommendations[element] = 'ARIA Recommendation: [img alt text]';
+        ariaRecommendations[element] = ariaObject.imageAlts;
     });
 
     // meta-viewport-text-zoom
     const metaViewportElements = await checkMetaViewportTextResize();
 
     metaViewportElements.forEach((element: string, index: number) => {
-        ariaRecommendations[element] = 'ARIA Recommendation: [meta-viewport-text-zoom]';
+        ariaRecommendations[element] = ariaObject.metaViewport;
       });
 
 
