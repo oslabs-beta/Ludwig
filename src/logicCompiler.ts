a0b0d3969778cf378cad32fd2faaee4f6eb8ce50
Jake Johnson <jkjohnson1996@gmail.com>
2023-11-27 19:47:28 -0800

    logic compiler compatability updates
    Co-authored-by: Tyler Spicer <tyler.e.spicer@gmail.com>

--- src/logicCompiler.ts
+++ src/logicCompiler.ts
@@ -1,7 +1,12 @@
 import * as vscode from 'vscode';
-const { evalAnchors } = require('./aria-standards/critical/anchor-labels.js'); // import anchor-labels file
+const { evalAnchors } = require('./aria-standards/critical/anchor-labels.js');
 const { checkAreaMapAltText } = require('./aria-standards/critical/area-maps-alt-text.js');
 const { checkAriaHidden } = require('./aria-standards/critical/aria-hidden.js');
+const { checkButtonText } = require('./aria-standards/critical/button-text.js');
+const { checkUniqueIds } = require('./aria-standards/critical/unique-ids.js');
+const { checkImgAltText } = require('./aria-standards/critical/img-alt-text.js');
+const { checkMetaViewportTextResize } = require('./aria-standards/critical/meta-viewport-text-zoom.js');
+
 
 export interface AriaRecommendations {
     [key: string]: string;
@@ -30,6 +35,35 @@ export async function compileLogic(document: vscode.TextDocument): Promise<AriaR
     hiddenAria.forEach((element: string, index: number) => {
         ariaRecommendations[element] = 'ARIA Recommendation: [info to be defined later]';
       });
+
+    // button-text
+    const buttonText = await checkButtonText();
+
+    buttonText.forEach((element: string, index: number) => {
+        ariaRecommendations[element] = 'ARIA Recommendation: [info to be defined later]';
+    });
+
+    // unique-ids
+    const duplicateElements = await checkUniqueIds();
+
+    duplicateElements.forEach((element: string, index: number) => {
+        ariaRecommendations[element] = 'ARIA Recommendation: [info to be defined later]';
+      });
+
+    // img alt text
+    const imgAlts = await checkImgAltText();
+    
+    imgAlts.forEach((element: string, index: number) => {
+        ariaRecommendations[element] = 'ARIA Recommendation: [info to be defined later]';
+    });
+
+    // meta-viewport-text-zoom
+    const metaViewportElements = await checkMetaViewportTextResize();
+
+    metaViewportElements.forEach((element: string, index: number) => {
+        ariaRecommendations[element] = 'ARIA Recommendation: [info to be defined later]';
+      });
+
 
     return ariaRecommendations;
 }
