40e1629799a9757266507916cb1ed49cd5eb893b
Jake Johnson <jkjohnson1996@gmail.com>
2023-11-27 19:48:35 -0800

    clean up

--- src/aria-standards/critical/meta-viewport-text-zoom.js
+++ src/aria-standards/critical/meta-viewport-text-zoom.js
@@ -1,37 +1,6 @@
 const vscode = require('vscode');
 const { JSDOM } = require('jsdom');
 
-// const htmlCode = `
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="utf-8" />
-//     <title>Sample Title</title>
-//     <meta http-equiv="refresh" content="0; URL=https://planet-express.example.com">
-//   </head>
-//   <body>
-//   <img src='someurlhere.com' alt='a cute photo' />
-//     <header aria-hidden="true">This is the header!</header>
-//     <div class="link container">
-//       <a href="https://www.example.com">Click me</a>
-//       <a aria-label="tag-2" href="https://www.example.com">Click me</a>
-//       <a aria-label="Click me" href="https://www.example.com">Click me</a>
-//     </div>
-//     <div id="duplicate">Something</div>
-//     <input id="duplicate" type="button">
-//     <div>GorbleGorble</div>
-//     <button name='button' text ="Hello There"></button>
-//   </body>
-// </html>
-// `;
-
-// const { window } = new JSDOM(htmlCode);
-// const document = window.document;
-// const ludwig = document.body;
-
-// // default message with specific aria-fail found and link to docs
-// const defaultMsg = {};
-
 // <meta name=”viewport”> does not disable text scaling and zooming
 function checkMetaViewportTextResize() {
   const activeEditor = vscode.window.activeTextEditor;
