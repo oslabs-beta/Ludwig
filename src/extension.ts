15e428ce936f250aef763c099b3f532cba3495c4
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-01 11:41:36 -0800

    add full html to webview

--- src/extension.ts
+++ src/extension.ts
@@ -159,7 +159,20 @@ export function activate(context: vscode.ExtensionContext) {
               enableScripts: true,  
             };
             webviewView.webview.html = `
-                <h1>HELLO LUDWIG!</h1>
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                </head>
+                <body>
+                    <h1>Hello, Ludwig</h1>
+                    <div id="root"></div>
+                    <script>
+                    window.vscodeApi = acquireVsCodeApi();
+                    </script>
+                </body>
+                </html>
             `;
         }
     }
