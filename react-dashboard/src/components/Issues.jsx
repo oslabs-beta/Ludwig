24d55bc91b7900dc6338277959fc753173037af9
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-07 18:57:23 -0800

    add code tag to element in Issues.jsx

--- react-dashboard/src/components/Issues.jsx
+++ react-dashboard/src/components/Issues.jsx
@@ -5,7 +5,7 @@ export default function Issues ({recommendations}) {
   const elements = Object.keys(recommendations).map((el) => {
     return (
     <>
-      <li key={crypto.randomUUID()}><b>Element: </b> {el}</li>
+      <li key={crypto.randomUUID()}><b>Element: </b> <code>{el}</code></li>
       <ul>
         <li>Recommendation: {recommendations[el]['desc']} (<a href={recommendations[el]['link']}>Read More</a>)</li>
       </ul>
