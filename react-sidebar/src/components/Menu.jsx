6b1951c2a87a50306a5d60354a4f9cdf708edf37
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-08 19:38:07 -0800

    sidebar button styling

--- react-sidebar/src/components/Menu.jsx
+++ react-sidebar/src/components/Menu.jsx
@@ -12,7 +12,7 @@ export default function Menu () {
   return (
     <div>
       <h3>Improve the accessibility of your website!</h3>
-      <p>Click the button below to scan the current HTML document and generate a report to enhance your code</p>
+      <p>Click the button below to scan the current HTML document and generate a report to enhance your code:</p>
       <button onClick={handleClick}>Scan Document</button>
     </div>
   );
