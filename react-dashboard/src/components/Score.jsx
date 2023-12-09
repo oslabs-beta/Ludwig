451d2a2e3ceb9c75603a27095a4c2e3d0436ca83
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-09 08:54:56 -0800

    minor styling of issue table

--- react-dashboard/src/components/Score.jsx
+++ react-dashboard/src/components/Score.jsx
@@ -2,10 +2,11 @@ import React, { useState, useRef } from 'react';
 
 export default function Score ({recommendations}) {
   return (
-    <div>
+    <div className='centered'>
       <div>
         <h3>Your Ludwig Accessibility Score:</h3>
         <p>*score here*</p>
+        {"\b"}
       </div>
       <div>
         <h3>Total Issues Found:</h3>
