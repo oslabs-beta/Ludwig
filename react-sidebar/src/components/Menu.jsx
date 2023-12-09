818a1f924ccdc4cbece772fa6ae0abf11e648bde
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-08 16:57:55 -0800

    add console log to debug Menu

--- react-sidebar/src/components/Menu.jsx
+++ react-sidebar/src/components/Menu.jsx
@@ -1,20 +1,19 @@
-import React, { useState, useRef } from 'react';
-// const button = document.querySelector('button');
-// button.addEventListener('click', () => {
-//     window.vscodeApi.postMessage({ message: 'scanDoc' });
-// });
+import React, { useState, useRef, useEffect } from 'react';
+
+
 export default function Menu () {
   const vscode = window.vscodeApi;
-  const handleClick = (event) => {
-    vscode.postMessage({ message: 'scanDoc' });
-  };
+
+    const handleClick = (event) => {
+      vscode.postMessage({ message: 'scanDoc' });
+      console.log('clicked');
+    };
 
   return (
     <div>
       <h3>Improve the accessibility of your website!</h3>
       <p>Click the button below to scan the current HTML document and generate a report to enhance your code</p>
       <button onClick={handleClick}>Scan Document</button>
-
     </div>
   );
 }
