9f40e7ac693485dcfe58e7991f02cad9b9c53d0f
Jake Johnson <jkjohnson1996@gmail.com>
2023-12-11 11:06:27 -0800

    multi line update

--- src/aria-standards/critical/form-labels.js
+++ src/aria-standards/critical/form-labels.js
@@ -1,7 +1,7 @@
 const vscode = require('vscode');
 const { JSDOM } = require('jsdom');
 
-function checkLabels() {
+async function checkLabels() {
   const activeEditor = vscode.window.activeTextEditor;
 
   if (activeEditor && activeEditor.document.languageId === 'html') {
@@ -37,24 +37,12 @@ function checkLabels() {
           const inputId = inputsArray[i].getAttribute('id');
           const labelFor = labelsArray[i].getAttribute('for');
           if (inputId !== labelFor) {
-
             // console.log('Offset: ', labelsArray[i].startOffset);
             // console.log('position obj: ', activeEditor.document.positionAt(labelsArray[i].startOffset));
             // const lineNumber = activeEditor.document.positionAt(labelsArray[i].startOffset).line;
             // console.log(lineNumber);
-            // formArray.push(labelsArray[i].outerHTML);
-            // const newInputStr = inputsArray[i].outerHTML.replace('>', ' />');
             formArray.push(labelsArray[i].outerHTML);
             // formArray.push(inputsArray[i].outerHTML);
-            // const outerHTMLContent = form.outerHTML;
-            // // console.log('outerHTMLContent: ', outerHTMLContent);
-            
-            // // snag the index of the first newline character
-            // const indexOfNewline = outerHTMLContent.indexOf('\n');
-
-            // // snag the first line using substring
-            // const firstLine = indexOfNewline !== -1 ? outerHTMLContent.substring(0, indexOfNewline) : outerHTMLContent;
-            // formArray.push(firstLine); // this handles the inputs and labels being put on separate lines!!!
           }
       }
     });
