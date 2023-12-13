b84b29fe3192d11256dd91b7cd860088762348b1
Jake Johnson <jkjohnson1996@gmail.com>
2023-12-13 10:38:19 -0800

    interface updated with dynamic score
    Co-authored-by: Prashay Mehta <wadesword@yahoo.com>
    Co-authored-by: Tyler Spicer <tyler.e.spicer@gmail.com>

--- react-dashboard/src/components/Score.jsx
+++ react-dashboard/src/components/Score.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useRef, memo } from 'react';
-import Panel from './Panel';
+import Bar from './Bar';
 
 function Score ({recommendations}) {
+
   return (
-    // <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
     <div>
       <h2>Your Ludwig Accessibility Score:</h2>
-      <Panel />
-      <h2>Total Issues Found:</h2>
-      <h1 className='circle'><span>{Object.keys(recommendations).length}</span></h1>
+      <Bar recommendations = {recommendations}/>
+      <h2>Total Accessibility Issues Found:</h2>
+      <h1 className='circle'><span>{Object.keys(recommendations.data).length}</span></h1>
     </div>
   );
 }
