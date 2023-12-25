7077d0c468941ed89285b527c1b1ba3ee52279e7
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-25 14:17:36 -0800

    add express routing

--- react-dashboard/src/server.js
+++ react-dashboard/src/server.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+
 const app = express();
+const port = 3695;
+
+app.use(express.json());
+app.use(cors());
+
+//get request to database 
+//app.get()
+
+//catch unknown routes
+app.use((req,res) => res.status(404).send('Page not found'));
+
+//global error handling
+app.use((err, req, res, next) => {
+  console.log('ERROR HERE: ', err);
+  const defaultErr = {
+    log: 'Express error hander caught unknown middleware error',
+    status: 500,
+    message: {err: 'An error occurred'}
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
