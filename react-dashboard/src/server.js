862164046e3fa397ec7daeb4c361f4b2ea7aeb0c
Prashay <113265973+wadesword89@users.noreply.github.com>
2024-01-06 15:49:25 -0800

    install cors

--- react-dashboard/src/server.js
+++ react-dashboard/src/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const PORT = 3000;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res) => {
+
+});
+
 
 app.listen(PORT,()=> {
   console.log(`Server listening at port ${PORT}`);
