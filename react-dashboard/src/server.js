1dd0dec681f90111f60aa29872877172faa0c724
Prashay <113265973+wadesword89@users.noreply.github.com>
2024-01-06 15:56:01 -0800

    add global error handling

--- react-dashboard/src/server.js
+++ react-dashboard/src/server.js
@@ -12,6 +12,18 @@ app.get('/',(req,res) => {
 
 });
 
+//Global error handling
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: {err: 'An error occurred'},
+  };
+  const errorObj = {...defaultErr, ...err};
+  console.log('error log:', errorObj.message);
+
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(PORT,()=> {
   console.log(`Server listening at port ${PORT}`);
