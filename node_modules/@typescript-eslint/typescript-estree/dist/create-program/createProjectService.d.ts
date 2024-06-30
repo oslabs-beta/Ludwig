c7eb9914fddf69841c20e6889757398e8416e686
artmontinski <artmontinski@gmail.com>
2024-06-30 11:02:56 -0700

    Removed node_modules

--- node_modules/@typescript-eslint/typescript-estree/dist/create-program/createProjectService.d.ts
+++
@@ -1,4 +0,0 @@
-import type * as ts from 'typescript/lib/tsserverlibrary';
-export type TypeScriptProjectService = ts.server.ProjectService;
-export declare function createProjectService(jsDocParsingMode?: ts.JSDocParsingMode): TypeScriptProjectService;
-//# sourceMappingURL=createProjectService.d.ts.map
